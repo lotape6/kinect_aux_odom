5bba71dad00afa36829fa2e2164f6547823af7d4
marrcogrova <marrcogrova@gmail.com>
2019-02-26 00:26:59 +0100

    iNIT

--- src/kimu_odom.cpp
+++ src/kimu_odom.cpp
@@ -1,6 +1,18 @@
-/*--------------------------------------------------------------------
- *
- *------------------------------------------------------------------*/
+ #include <ros/ros.h>
+ #include <ros/console.h>
+ #include <string>
+ #include <iostream>
+ #include <sensor_msgs/Imu.h>
+ #include <geometry_msgs/Pose.h>
+ #include <geometry_msgs/PoseStamped.h>
+ // #include <boost/bind.hpp>
+
+void imuCb(const sensor_msgs::ImuConstPtr& imu_msg){
+
+  ROS_INFO_STREAM("data x: " << imu_msg->linear_acceleration.x);
+  ROS_INFO_STREAM("data y: " << imu_msg->linear_acceleration.y);
+  ROS_INFO_STREAM("data z: " << imu_msg->linear_acceleration.z);
+}
 
 int main(int argc, char **argv)
 {
@@ -8,7 +20,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "kimu_odom");
   ros::NodeHandle n;
 
-
+  ros::Subscriber sub = n.subscribe("Imu", 100, imuCb);
 
   // Main loop.
   while (n.ok())
@@ -16,7 +28,7 @@ int main(int argc, char **argv)
     // Publish the message.
 
     ros::spinOnce();
-    r.sleep();
+    //r.sleep();
   }
 
   return 0;
